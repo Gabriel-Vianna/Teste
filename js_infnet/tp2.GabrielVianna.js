6d54683a2e8760313c3635517258e089917ce473
Gabriel Vianna <36870716+Gabriel-Vianna@users.noreply.github.com>
2020-07-17 18:27:12 -0300

    Teste

---
+++ js_infnet/tp2.GabrielVianna.js
@@ -0,0 +1,61 @@
+// Gabriel Andrade Vianna
+
+function questao1() {
+    // qtd 2 e 3 entre 0 e 1000000
+  
+  }
+  
+  function questao2() {
+    let resultado
+    for(let i; i=53;i--){
+        resultado = resultado * i
+    }
+    console.log('Iniciando Algoritmo Nr 02')
+    return resultado
+  }
+  
+  console.log('teste')
+  
+  function questao3() {
+    let aluno = [
+      {nr: 1, nota: 10},
+      {nr: 2, nota: 10},
+      {nr: 3, nota: 10},
+      {nr: 4, nota: 10},
+      {nr: 5, nota: 10},
+      /// ate 20
+      {nr: 20, nota: 10}
+    ]
+  
+  
+    // para cada aluno --->   Nota yy [(A/RE)PROVADO]"
+    // no final "APROVADOS: XX (xx%) | REPROVADOS: YY (yy%)"
+  }
+  
+  function questao4() {
+    let numeros = []
+    let tentativas = 0;
+  
+    // loop
+  
+    console.log(tentativas)
+  }
+  
+  
+  function questao5() {
+  
+    function tipoTriangulo(a, b, c) {
+      return ''
+    }
+  
+    console.log(tipoTriangulo(5, 5, 5))
+    console.log(tipoTriangulo(45,45,90))
+    console.log(tipoTriangulo(25, 35, 45))
+  }
+  
+  
+//   questao1()
+  console.log(questao2())
+//   questao3()
+//   questao4()
+//   questao5()
